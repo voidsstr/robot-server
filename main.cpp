9ab7a9e2f9471e83adc528076938c34699fdbafc
mike <perrymb@gmail.com>
2016-01-22 14:32:12 +1030

    networking working

--- main.cpp
+++ main.cpp
@@ -1,37 +1,69 @@
+#include <cstdlib>
 #include <iostream>
-
-#include <ctime>
-#include <iostream>
-#include <string>
+#include <thread>
+#include <utility>
 #include <boost/asio.hpp>
 
 using boost::asio::ip::tcp;
 
-using namespace std;
+const int max_length = 1024;
 
-int main()
+void session(tcp::socket sock)
 {
+  try
+  {
+    for (;;)
+    {
+      char data[max_length];
+
+      boost::system::error_code error;
+      size_t length = sock.read_some(boost::asio::buffer(data), error);
+      if (error == boost::asio::error::eof)
+        break; // Connection closed cleanly by peer.
+      else if (error)
+        throw boost::system::system_error(error); // Some other error.
+
+      boost::asio::write(sock, boost::asio::buffer(data, length));
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception in thread: " << e.what() << "\n";
+  }
+}
+
+void server(boost::asio::io_service& io_service, unsigned short port)
+{
+  tcp::acceptor a(io_service, tcp::endpoint(tcp::v4(), port));
+  for (;;)
+  {
+    tcp::socket sock(io_service);
+    a.accept(sock);
+    printf("Recieved data");
+    std::thread(session, std::move(sock)).detach();
+  }
+}
+
+//TODO: define packet handling mechanism for controlling robot remotely
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 2)
+    {
+      std::cerr << "Usage: blocking_tcp_echo_server <port>\n";
+      return 1;
+    }
+
     boost::asio::io_service io_service;
 
-    try
-    {
-        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 13));
+    server(io_service, std::atoi(argv[1]));
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
 
-        for(;;)
-        {
-            tcp::socket socket(io_service);
-
-            acceptor.accept(socket);
-
-            std::string message = "test";
-
-            boost::system::error_code ignored_error;
-
-            boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
-        }
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
+  return 0;
 }
